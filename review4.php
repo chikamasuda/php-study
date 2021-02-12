5bf3e5b2c36200ea1a73392a31c6cfdd1b602947
chikamasuda <c.m.062820@gmail.com>
2021-02-13 02:45:18 +0900

    review4のコード修正

--- review4.php
+++ review4.php
@@ -1,11 +1,11 @@
 <?php
 for($i=1; $i<=100; $i++) {
-  if($i % 3 === 0) {
-    echo 'Fizz';
+  if(($i % 3 === 0) && ($i % 5 === 0)) {
+    echo 'FizzBuzz';
   } else if ($i % 5 === 0) {
     echo 'Buzz';
-  } else if ($i % 15 === 0) {
-    echo 'FizzBuzz';
+  } else if ($i % 3 === 0) {
+    echo 'Fizz';
   } else {
     echo $i;
   }
